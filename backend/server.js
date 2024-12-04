166a20cd87069eaf7e222102550879806e796569
Sowmiyasaravanan-13 <160233030+Sowmiyasaravanan-13@users.noreply.github.com>
2024-12-04 11:35:20 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ const connectDatabase = require('./config/database');
 
 connectDatabase();
 
-const server = app.listen(process.env.PORT,()=>{
-    console.log(`My Server listening to the port: ${process.env.PORT} in  ${process.env.NODE_ENV} `)
+const server = app.listen(process.env.PORT || 5001,()=>{
+    console.log(`My Server listening to the port: ${process.env.PORT || 5001}`)
 })
 
 process.on('unhandledRejection',(err)=>{
